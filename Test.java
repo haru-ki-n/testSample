c12e4a1da3fe1f020b995c0b80d1febf0d92e6c6
haru-ki-n <ruriharuki@gmail.com>
2023-01-29 22:54:27 +0900

    ラムダサンプルコード追加

--- Test.java
+++ Test.java
@@ -1,3 +1,13 @@
 public class Test {
-    
+    public static void main(String[] args) {
+
+        Runnable runner = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello Lambda!");
+            }
+        };
+
+        runner.run(); // Hello Lambda!
+    }
 }
